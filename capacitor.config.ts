4386ab323e2d567c68b2120bcb93fdaff0e9e9da
cjpmd <cmcdonald002@dundee.ac.uk>
2026-04-21 17:01:15 +0100

    Change app name and scheme to lowercase

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.cb1a7443dfe74413bc7a813cf6770aa3',
-  appName: 'Player-Analysis',
+  appName: 'player-analysis',
   webDir: 'dist',
   ios: {
     contentInset: 'always',
-    scheme: 'Player Analysis',
+    scheme: 'player-analysis',
     backgroundColor: '#ffffff',
   },
   plugins: {
