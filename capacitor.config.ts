9bcb5468361f9ad7d58ab8652c0439a5b81548c5
cjpmd <cmcdonald002@dundee.ac.uk>
2026-04-21 16:48:13 +0100

    Remove server configuration from capacitor.config.ts
    Removed server configuration from Capacitor config.
    
    Added dist - removed server url

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.lovable.cb1a7443dfe74413bc7a813cf6770aa3',
   appName: 'Player-Analysis',
-  server: {
-    url: 'https://cb1a7443-dfe7-4413-bc7a-813cf6770aa3.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
+  webDir: 'dist',
   ios: {
     contentInset: 'always',
     scheme: 'Player Analysis',
