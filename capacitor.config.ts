8977bcf85ca7a4e458e9f3c5caf28e0e0e46bf5b
cjpmd <cmcdonald002@dundee.ac.uk>
2026-04-21 15:47:04 +0100

    Rename appName to 'Player-Analysis'
    Removed webDir: 'dist'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.cb1a7443dfe74413bc7a813cf6770aa3',
-  appName: 'Player Analysis',
-  webDir: 'dist',
+  appName: 'Player-Analysis',
   server: {
     url: 'https://cb1a7443-dfe7-4413-bc7a-813cf6770aa3.lovableproject.com?forceHideBadge=true',
     cleartext: true,
